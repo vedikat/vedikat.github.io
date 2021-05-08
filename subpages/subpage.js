edd5dc7391d3e257c8130728bc23a87dca9fd7fa
Vedika Bhatnagar <52089706+vedikat@users.noreply.github.com>
2021-05-07 22:25:27 -0400

    Update subpage.js

--- subpages/subpage.js
+++ subpages/subpage.js
@@ -14,14 +14,3 @@ classes.forEach(clss => {
         }
     })
 })
-const img_slider = document.querySelector('img_slider');
-const images = [
-    'https://cdn.wallpapersafari.com/95/31/rG4sVg.jpg',
-    'https://upload.wikimedia.org/wikipedia/commons/7/72/644x364_JerseyClose.jpg',
-    'https://lh3.googleusercontent.com/proxy/FYVurBsQXLIA9ar1DUMgOnW03s3QGLVkgbDnVifU6IBgy8yPGDxlLxfWezNqzzuZv22oTkcBmDKrZpPZS0bA1S72uSGql69x3BNqcLvqSIl9ZzqJ0-hV1Yr9J4i2MA',
-    'http://img.weburbanist.com/wp-content/uploads/2017/09/concrete-3D-printing-644x364.jpg'
-];
-
-function changeImage(){
-    img_slider.style.backgroundImage = 
-}
